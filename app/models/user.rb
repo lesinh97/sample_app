ed4202a5d6215396d17995e8f3432f7ee68a81a1
LeDSinh <dinhsinh.le@outlook.com>
2018-06-06 23:36:17 +0700

    chap_6

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/is
+  validates :email, format: {with: VALID_EMAIL_REGEX}
+  validates :email, presence: true, uniqueness: {case_sensitive: false},
+    length: {maximum: Settings.max_lenght_email}
+  validates :password, presence: true, length: {minimum: Settings.min_lenght_pass}
+  validates :name, presence: true, length: {maximum: Setting.max_lenght_name}
+  has_secure_password
+end
