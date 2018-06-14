5b9d1a084daf1c8370f4f8d7360ac4dda1f61333
LeDSinh <dinhsinh.le@outlook.com>
2018-06-14 16:22:01 +0700

    edit gemfile again

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,14 +18,12 @@ User.create!(name:  "Example User",
                activated: true,
                activated_at: Time.zone.now)
 end
-
 # Microposts
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
-
 # Following relationships
 users = User.all
 user  = users.first
